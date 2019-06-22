dbe0495fa77334952e12dd1b4b3164524988c9a6
Arnold <sebastianbnec@gmail.com>
2019-06-23 00:18:24 +0700

    added example resources

--- src/app.ts
+++ src/app.ts
@@ -6,3 +6,5 @@ const port = 3000;
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log('App is listening on port ', port));
+
+export default app;
---
+++ src/resources/index.ts
@@ -0,0 +1,16 @@
+interface questionAnserDictionary {
+    question: string,
+    answer: string[],
+}
+
+
+const sql: questionAnserDictionary[] = [
+    {
+        question: 'example1',
+        answer: ['yes', 'true']
+    },
+    {
+        question: 'example2',
+        answer: ['no', 'false']
+    },
+];
